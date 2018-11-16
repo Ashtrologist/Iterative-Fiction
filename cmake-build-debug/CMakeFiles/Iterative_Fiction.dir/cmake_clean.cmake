7981369b3af86a6133951d44665549b5825c2f48
Unknown <ashtonrodriquez@hotmail.com>
2018-11-16 16:27:09 -0500

    Created StoryTokenizer.h

---
+++ cmake-build-debug/CMakeFiles/Iterative_Fiction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Iterative_Fiction.dir/main.cpp.o"
+  "Iterative_Fiction.pdb"
+  "Iterative_Fiction"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Iterative_Fiction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
