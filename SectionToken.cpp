5ff8e65972a4f4cd58efea7e1fa2a785501096d9
Thiago Olabarriaga <thiago.olabarriaga@gmail.com>
2018-11-25 22:28:21 -0500

    Created SectionToken  Class

---
+++ SectionToken.cpp
@@ -0,0 +1,16 @@
+#include "SectionToken.h"
+#include <iostream>
+using namespace std;
+
+
+
+
+string SectionToken::getText()
+{
+	return string();
+}
+
+SectionToken::SectionToken(string, command_t)
+{
+
+}
