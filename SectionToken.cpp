691fdf783f373a734c1bb553ddadd29696f35380
Thiago Olabarriaga <thiago.olabarriaga@gmail.com>
2018-11-27 17:25:07 -0500

    Finished the Command class and subclasses except for Block because I don't understand what it is supposed to do

---
+++ SectionToken.cpp
@@ -0,0 +1,155 @@
+#include "SectionToken.h"
+#include <iostream>
+using namespace std;
+
+
+
+SectionToken::SectionToken(string& section)
+{
+	sectionText = section;
+}
+
+string SectionToken::getText() const{
+
+	return string();
+}
+
+
+
+Command::Command()
+{
+	text = "";
+	type = NULLT;
+}
+
+//Command Class Functions
+Command::Command(SectionToken& st)//constructor
+{
+	text = st.getText();
+	type = NULLT;
+}
+
+string Command::getText() const
+{
+	return text;
+}
+
+command_t Command::getType() const
+{
+	return type;
+}
+
+
+
+
+Link::Link(SectionToken & stok){//Working on LINK class ***************//
+
+	type = LINK;
+	location = 0;
+	if (stok.getText().find("-&gt;", location) != string::npos)
+
+	{
+
+		location = stok.getText().find("-&gt;", location);
+		text = stok.getText().substr(2, location-2);
+
+		location += 5;
+
+		sectionName = stok.getText().substr(location, stok.getText().length() - location - 2);
+
+	}
+
+	else {
+
+		text = stok.getText().substr(2, stok.getText().length() - 4);
+
+		sectionName = text;
+
+	}
+}
+
+string Link::getPassageName() const
+{
+	return sectionName;
+}
+
+Text::Text(SectionToken& stok){
+	type = TEXT;
+}
+
+Goto::Goto(SectionToken& stok){
+	type = GOTO;
+	location = stok.getText().find("&quot;") + 6;
+	sectionName = stok.getText().substr(location, stok.getText().find("&quot;", location) - location);
+}
+
+string Goto::getPassageName() const
+{
+	return sectionName;
+}
+
+Set::Set(SectionToken & stok)
+{
+	type = SET;
+	location = stok.getText().find("$") + 1;
+	text = stok.getText().substr(location, stok.getText().find(" to ", location) - location);
+
+	location = stok.getText().find(" to ", location) + 4;
+	if (stok.getText().substr(location, 4) == "true") {
+		setBool = true;
+	}
+
+	else {
+		setBool = false;
+	}
+}
+
+bool Set::getBool() const
+{
+	return setBool;
+}
+
+If::If(SectionToken & stok)
+{
+	type = IF;
+	location = stok.getText().find("$") + 1;
+	text = stok.getText().substr(location, stok.getText().find(" is ", location) - location);
+
+	location = stok.getText().find(" is ", location) + 4;
+	if (stok.getText().substr(location, 4) == "true") {
+		checkSet = true;
+	}
+	else {
+		checkSet = false;
+	}
+}
+
+bool If::getSetValue() const
+{
+	return checkSet;
+}
+
+Elseif::Elseif(SectionToken & stok)
+{
+	type = ELSEIF;
+	location = stok.getText().find("$") + 1;
+	text = stok.getText().substr(location, stok.getText().find(" is ", location) - location);
+
+	location = stok.getText().find(" is ", location) + 4;
+	if (stok.getText().substr(location, 4) == "true") {
+		checkSet = true;
+	}
+	else {
+		checkSet = false;
+	}
+}
+
+bool Elseif::getSetValue() const
+{
+	return checkSet;
+}
+
+Else::Else(SectionToken & stok)
+{
+	type = ELSE;
+}
--- SectionToken.h
+++ SectionToken.h
@@ -3,20 +3,109 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-enum command_t { LINK, GOTO, SET, IF, ELSEIF, ELSE, BLOCK, TEXT };
+enum command_t {NULLT, LINK, GOTO, SET, IF, ELSEIF, ELSE, BLOCK, TEXT };
 
 class SectionToken
 {
-private:
+protected:
 	string sectionText;
-	command_t sectionType;
 
 public:
 	string getText() const;
-	command_t getType() const;
-	SectionToken(string sentence, command_t type);
+	SectionToken(string& section);
 };
 
+
+//SUPERCLASS
+class Command{
+protected:
+	string text;
+	command_t type;
+	int location;
+
+public:
+	Command();
+	Command(SectionToken& st);
+	string getText() const;
+	command_t getType() const;
+};
+
+//**********SUBCLASSES************//
+class Link : public Command
+{
+private:
+    string sectionName;
+public:
+    Link(SectionToken& stok);
+    string getPassageName() const;
+};
+
+class Block : public Command
+{
+private:
+    vector<SectionToken*> blockSections;
+    int blockIndex;
+public:
+    Block(SectionToken& stok);
+    void addSection(SectionToken* blockSect) const;
+};
+
+class Text : public Command
+{
+public:
+    Text(SectionToken& stok);
+};
+
+class Goto : public Command
+{
+private:
+    string sectionName;
+public:
+    Goto(SectionToken& stok);
+    string getPassageName() const;
+};
+
+class Set : public Command
+{
+private:
+    bool setBool;
+public:
+    Set(SectionToken& stok);
+    bool getBool() const;
+};
+
+class If : public Command
+{
+private:
+    bool checkSet;
+public:
+    If(SectionToken& stok);
+    bool getSetValue() const;
+};
+
+class Elseif : public Command
+{
+private:
+    bool checkSet;
+public:
+    Elseif(SectionToken& stok);
+    bool getSetValue() const;
+};
+
+class Else : public Command
+{
+public:
+    Else(SectionToken& stok);
+};
+
+class BlockTokenizer
+{
+public:
+    BlockTokenizer();
+    bool hasNextSection(Block& block);
+    void nextSection(Block& block);
+};
 #endif
